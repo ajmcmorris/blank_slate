e2e44c6379448aac754a6238784557ad609d8f43
andre <amcmorris@deckerip.com>
2016-05-01 18:27:36 -0400

    responsive bug fixes

--- blank_space/src/assets/js/app.js
+++ blank_space/src/assets/js/app.js
@@ -1,1 +1,23 @@
 $(document).foundation();
+$(function(){
+	$('.grid .subheader').hide();
+
+	$('.effect-bubba').hover(function(){
+		$(this).find('.subheader').show();
+	},function(){
+		$(this).find('.subheader').hide();
+	});
+	console.log('jquery running');
+});
+$(window).bind("load", function () {
+    var footer = $("#footer");
+    var pos = footer.position();
+    var height = $(window).height();
+    height = height - pos.top;
+    height = height - footer.height();
+    if (height > 0) {
+        footer.css({
+            'margin-top': height + 'px'
+        });
+    }
+});
